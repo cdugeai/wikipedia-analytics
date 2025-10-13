063e0eb92850cd1014bd6f7c9d9424d793e1e974
Colin Dugeai <colin.dugeai@gmail.com>
2025-10-14 00:05:53 +0200

    feat: pyspark implementation
    faster than polars on the whole dataset

---
+++ python-pyspark/README.md
@@ -0,0 +1,12 @@
+# Pyspark implementation
+
+## Usage
+
+Run `make run` to execute the script.
+
+## Results
+
+Count infoboxes:
+
+- Single file (1.8GB):  6.9s
+- All files (32GB): 28s
--- python-pyspark/main.py
+++ python-pyspark/main.py
@@ -1,4 +1,15 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    ArrayType,
+)
+import pyspark.sql.functions as pys
 import Timing
+
+
+spark = SparkSession.builder.appName("Infobox counter").getOrCreate()
 
 t = Timing.Timing()
 
@@ -8,10 +19,35 @@ FILE_DATA1 = "../data/frwiki_namespace_0/frwiki_namespace_0_0.jsonl"
 FILE_DATA_ALL = "../data/frwiki_namespace_0/frwiki_namespace_0_*.jsonl"
 
 
-t.start("count_infobox")
-## TODO
+# Define schema (only fields you need)
+schema = StructType(
+    [
+        StructField(
+            "infoboxes",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("name", StringType(), True),
+                        StructField("type", StringType(), True),
+                    ]
+                )
+            ),
+            True,
+        )
+    ]
+)
 
-count_infobox = None
+t.start("count_infobox")
+
+df = spark.read.schema(schema).json(FILE_DATA_ALL)
+
+count_infobox = (
+    df.select(pys.col("infoboxes")[0]["name"].alias("infobox1_name"))
+    .groupBy(pys.col("infobox1_name"))
+    .agg(pys.count("*").alias("n_articles"))
+    .orderBy(pys.col("n_articles").desc())
+)
 
 t.stop()
+count_infobox.write.mode("overwrite").csv("out/count_infobox.csv", header=True)
 print(count_infobox)
