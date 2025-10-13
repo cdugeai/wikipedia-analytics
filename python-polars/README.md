efccd047876801c83f9dcf9e0ed2ef0614c52799
Colin Dugeai <colin.dugeai@gmail.com>
2025-10-13 23:12:39 +0200

    feat: use restricted schema

--- python-polars/README.md
+++ python-polars/README.md
@@ -3,3 +3,10 @@
 ## Usage
 
 Run `make run` to execute the script.
+
+## Results
+
+Count infoboxes:
+
+- Single file (1.8GB):  4.6s
+- All files (32GB): 60s
--- python-polars/main.py
+++ python-polars/main.py
@@ -5,7 +5,14 @@ FILE_SAMPLE = "../data/frwiki_namespace_0/sample.jsonl"
 FILE_DATA1 = "../data/frwiki_namespace_0/frwiki_namespace_0_0.jsonl"
 FILE_DATA_ALL = "../data/frwiki_namespace_0/frwiki_namespace_0_*.jsonl"
 
-df = pl.scan_ndjson(FILE_DATA_ALL)
+# Json properties to keep (speed-up parsing)
+schema_ = {
+    "name": pl.Utf8,
+    "infoboxes": pl.List(
+        pl.Struct([pl.Field("name", pl.Utf8), pl.Field("type", pl.Utf8)])
+    ),
+}
+df = pl.scan_ndjson(FILE_DATA_ALL, schema=schema_)
 
 count_infobox = (
     df.select(pl.col("infoboxes"))
@@ -16,8 +23,8 @@ count_infobox = (
     )
     .select(pl.col("infobox1_name"))
     .group_by(pl.col("infobox1_name"))
-    .len(name="n")
-    # .sort(pl.col("n"), descending=True)
+    .len(name="n_articles")
+    .sort(pl.col("n_articles"), descending=True)
 ).collect(engine="streaming")
 
 print(count_infobox)
