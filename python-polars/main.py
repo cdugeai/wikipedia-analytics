6a7ef9f4f60d00cf276f5869af2922d966913fab
Colin Dugeai <colin.dugeai@gmail.com>
2025-10-13 12:33:32 +0200

    refactor: clean code
    oom error on all files

--- python-polars/main.py
+++ python-polars/main.py
@@ -3,6 +3,7 @@ import polars as pl
 
 FILE_SAMPLE = "../data/frwiki_namespace_0/sample.jsonl"
 FILE_DATA1 = "../data/frwiki_namespace_0/frwiki_namespace_0_0.jsonl"
+FILE_DATA_ALL = "../data/frwiki_namespace_0/frwiki_namespace_0_*.jsonl"
 
 
 df = pl.scan_ndjson(FILE_SAMPLE)
@@ -12,11 +13,14 @@ count_infobox = (
     df.select(pl.col("infoboxes"))
     .with_columns(
         infobox_len=pl.col("infoboxes").len(),
-        name_=pl.col("infoboxes").list.first().struct.field("name"),
+        # Name of the first infobox of the article
+        infobox1_name=pl.col("infoboxes").list.first().struct.field("name"),
     )
-    .group_by(pl.col("name_"))
+    .select(pl.col("infobox1_name"))
+    .group_by(pl.col("infobox1_name"))
     .len(name="n")
-    .sort(pl.col("n"), descending=True)
-)
+    # .sort(pl.col("n"), descending=True)
+).collect()
 
-print(count_infobox.collect())
+print(count_infobox)
+count_infobox.write_csv("out/count_infobox.csv")
