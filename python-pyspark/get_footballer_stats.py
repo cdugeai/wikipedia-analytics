57e3b38bd26a0a0f5f3097e1e5d0dbf85be8314e
Colin Dugeai <colin.dugeai@gmail.com>
2025-10-16 11:40:50 +0200

    feat: bucket countries in authorized list

--- python-pyspark/get_footballer_stats.py
+++ python-pyspark/get_footballer_stats.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 import Timing
 import pyspark.sql.functions as pys
+from pyspark.sql.window import Window
 
 
 spark = SparkSession.builder.appName("Footballer stats").getOrCreate()
@@ -19,6 +20,10 @@ df = (
     .withColumn("country", pys.split(pys.col("country"), " ")[0])
 )
 
+# Allowed country names, fr-en
+countries_ref = spark.read.csv(
+    "../data/countries_ref.csv", header=True
+).withColumnRenamed("french", "country_ref")
 
 footballers_stats: DataFrame = (
     df.select("country", "height", "foot", "is_alive")
@@ -27,12 +32,39 @@ footballers_stats: DataFrame = (
         pys.avg(pys.col("height")).alias("avg_height"),
         pys.count("*").alias("n_players"),
     )
-    .orderBy("country", "foot")
+    .crossJoin(countries_ref)
+    # Filter when 3 first letters match
+    .filter(pys.substring("country", 0, 4) == pys.substring("country_ref", 0, 4))
+    .withColumn(
+        "distance",
+        pys.levenshtein(
+            pys.lower(pys.col("country")), pys.lower(pys.col("country_ref"))
+        ),
+    )
+    .withColumn(
+        "rank",
+        pys.row_number().over(
+            Window.partitionBy("country", "foot").orderBy("distance")
+        ),
+    )
+    # Keep closest country name
+    .filter(pys.col("rank") == 1)
+    # Use column "english" as ref
+    .withColumn("country_ref", pys.col("english"))
+    .groupBy(["country_ref", "foot"])
+    .agg(
+        pys.sum("n_players").alias("n_players"),
+        (
+            pys.sum((pys.col("n_players") * pys.col("avg_height")))
+            / pys.sum("n_players")
+        ).alias("avg_height"),
+    )
+    .filter(pys.col("foot").isin(["left", "right", "both"]) | pys.col("foot").isNull())
+    # .select("country", "country_ref", "distance")
+    .orderBy("country_ref", "foot")
 )
 
-footballers_stats.show()
 # footballers_stats.explain()
 
-# print(footballers_stats.collect()[0])
 footballers_stats.write.mode("overwrite").csv("out/footballers_stats.csv", header=True)
 t.stop()
