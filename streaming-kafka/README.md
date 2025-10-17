2d8ca044b25c114b63bbc45b28e6084be4c7e62b
Colin Dugeai <colin.dugeai@gmail.com>
2025-10-17 17:30:54 +0200

    wip: try to have flink working

--- streaming-kafka/README.md
+++ streaming-kafka/README.md
@@ -11,3 +11,14 @@ make kafka
 make streaming
 
 ```
+
+## Installation
+
+```sh
+cd jars
+wget https://repo.maven.apache.org/maven2/org/apache/flink/flink-connector-kafka/3.1.0-1.18/flink-connector-kafka-3.1.0-1.18.jar
+
+or
+
+wget https://repo1.maven.org/maven2/org/apache/flink/flink-sql-connector-kafka/3.1.0-1.18/flink-sql-connector-kafka-3.1.0-1.18.jar
+```
---
+++ streaming-kafka/analysis.py
@@ -0,0 +1,34 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.datastream.functions import MapFunction
+from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer
+from pyflink.common.serialization import SimpleStringSchema
+from pyflink.common.typeinfo import Types
+import json
+import os
+
+class PrintFunction(MapFunction):
+    def map(self, value):
+        data = json.loads(value)
+        print(f"Title: {data.get('title')}, User: {data.get('user')}")
+        return value
+
+env = StreamExecutionEnvironment.get_execution_environment()
+#env.add_jars("/tmp/flink-connector-kafka-3.0.0-1.17.jar")
+jar_path = os.path.abspath("./jars/flink-connector-kafka-3.1.0-1.18.jar")
+print(jar_path)
+env.add_jars(f"file:///{jar_path}")
+
+
+# Create Kafka consumer
+kafka_consumer = FlinkKafkaConsumer(
+    topics='wiki_data',
+    deserialization_schema=SimpleStringSchema(),
+    properties={'bootstrap.servers': 'localhost:9092', 'group.id': 'flink-group'}
+)
+
+# Add source and apply the print function
+ds = env.add_source(kafka_consumer)
+ds.map(PrintFunction()).print()
+
+# Execute the job
+env.execute("Flink Kafka Consumer")
