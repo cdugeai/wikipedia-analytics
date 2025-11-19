3337d742dd0b3f7e8c76b9f890489aff410a5292
Colin Dugeai <46027833+cdugeai@users.noreply.github.com>
2025-11-19 17:00:15 +0100

    chore: Scala 2.12 works but terrible syntax

--- streaming-flink-scala/build.sbt
+++ streaming-flink-scala/build.sbt
@@ -1,13 +1,14 @@
 val scala3Version = "3.6.2"
-val flinkVersion = "2.1.1" // Matching Flink cluster's version
+val flinkVersion = "1.20.3" // Matching Flink cluster's version
+val scala2Version = "2.12.18"
 
 lazy val root = project
   .in(file("."))
   .settings(
     name := "streaming-flink-scala",
-    version := "0.1.0",
+    version := "0.2.0",
 
-    scalaVersion := scala3Version,
+    scalaVersion := scala2Version,
 
     libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test,
 
@@ -15,14 +16,20 @@ lazy val root = project
 
 libraryDependencies ++= Seq(
   // [ SCALA API ] Flink deps not compatible with Scala 3
-  //"org.apache.flink" %% "flink-scala" % flinkVersion % "provided",
-  //"org.apache.flink" %% "flink-streaming-scala" % flinkVersion % "provided",
-  //"org.apache.flink" %% "flink-clients" % flinkVersion % "provided",
+  "org.apache.flink" %% "flink-scala" % flinkVersion % "provided",
+  "org.apache.flink" %% "flink-streaming-scala" % flinkVersion % "provided",
+  // [ JAVA API ] Flink deps not compatible with Scala 3
+  //"org.apache.flink" % "flink-clients" % flinkVersion % "provided",
+
+
+  //"org.flinkextended" %% "flink-scala-api" % "1.20.0_1.2.0",
+  //"org.apache.flink" % "flink-clients" % "1.20.3"
+
 
   // [ JAVA API ] Use Java APIs (no %%)
-  "org.apache.flink" % "flink-core" % flinkVersion % "provided",
-  "org.apache.flink" % "flink-streaming-java" % flinkVersion % "provided",
-  "org.apache.flink" % "flink-clients" % flinkVersion % "provided",
+  //"org.apache.flink" % "flink-core" % flinkVersion % "provided",
+  //"org.apache.flink" % "flink-streaming-java" % flinkVersion % "provided",
+  //"org.apache.flink" % "flink-clients" % flinkVersion % "provided",
   //"org.apache.flink" % "flink-connector-kafka" % flinkVersion
 )
 
--- streaming-flink-scala/src/main/scala/WordCount.scala
+++ streaming-flink-scala/src/main/scala/WordCount.scala
@@ -1,48 +1,41 @@
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-import org.apache.flink.util.Collector
-import org.apache.flink.api.common.functions.FlatMapFunction
-import org.apache.flink.api.java.tuple.Tuple2
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.api.common.functions.{FlatMapFunction, FilterFunction, MapFunction}
 import org.apache.flink.api.java.functions.KeySelector
+import org.apache.flink.util.Collector
 
 object WordCountJob {
   def main(args: Array[String]): Unit = {
-    // get the environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
-    // sample input (replace with env.readTextFile(...) or socket stream as needed)
-    val text = env.fromElements(
+    val text: DataStream[String] = env.fromElements(
       "To be, or not to be: that is the question",
       "Whether 'tis nobler in the mind to suffer",
       "To die, to sleep"
     )
 
-    // FlatMap: split lines into (word, 1) tuples
-    val words = text.flatMap(new FlatMapFunction[String, Tuple2[String, Integer]] {
-      override def flatMap(value: String, out: Collector[Tuple2[String, Integer]]): Unit = {
-        val tokens = value.toLowerCase.split("\\W+")
-        var i = 0
-        while (i < tokens.length) {
-          val t = tokens(i)
-          if (t.nonEmpty) {
-            // note: use java.lang.Integer (boxed) for Flink Tuple2<int> semantics
-            out.collect(new Tuple2(t, Integer.valueOf(1)))
+    val counts = text
+      .flatMap(new FlatMapFunction[String, String] {
+        override def flatMap(value: String, out: Collector[String]): Unit = {
+          val words = value.toLowerCase.split("\\W+")
+          var i = 0
+          while (i < words.length) {
+            if (words(i).nonEmpty) {
+              out.collect(words(i))
+            }
+            i += 1
           }
-          i += 1
         }
-      }
-    })
-
-    // Key by the word (f0) and sum the counts (f1)
-    val counts = words
-      .keyBy(new KeySelector[Tuple2[String, Integer], String] {
-        override def getKey(value: Tuple2[String, Integer]): String = value.f0
+      })
+      .map(new MapFunction[String, (String, Int)] {
+        override def map(value: String): (String, Int) = (value, 1)
+      })
+      .keyBy(new KeySelector[(String, Int), String] {
+        override def getKey(value: (String, Int)): String = value._1
       })
       .sum(1)
 
-    // print to stdout
     counts.print()
-
-    env.execute("Scala 3 WordCount (using Java DataStream API)")
+    env.execute("Scala 2 WordCount")
   }
 }
