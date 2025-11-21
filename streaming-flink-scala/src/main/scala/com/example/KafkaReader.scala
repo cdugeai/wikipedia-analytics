d7a3ff6aeb285481c30a979c3080a427dae94ea2
Colin Dugeai <colin.dugeai@gmail.com>
2025-11-21 17:45:54 +0100

    feat: flink job group wiki updates done

--- streaming-flink-scala/src/main/scala/com/example/KafkaReader.scala
+++ streaming-flink-scala/src/main/scala/com/example/KafkaReader.scala
@@ -1,12 +1,45 @@
 package com.example
 
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+
 import com.example.model.WikipediaUpdate
 import com.example.utils.JsonDeserializer
 import org.apache.flink.connector.kafka.source.KafkaSource
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner
+import com.fasterxml.jackson.module.scala.deser.overrides
+
+// Type for the gathered results
+case class WindowResult(windowStart: Long, windowEnd: Long, wiki: String, totalUpdates: Int, distinctUsers: Int)
+
+// Method to gather results by wiki
+class GatherRecords extends ProcessWindowFunction[WikipediaUpdate, WindowResult, String, TimeWindow] {
+  override def process(
+    key: String,
+    context: Context,
+    elements: Iterable[WikipediaUpdate],
+    out: Collector[WindowResult]
+  ): Unit = {
+    val totalUpdates = elements.map(_ => 1).sum
+    // val users = elements.map(_.user).mkString(", ") // Concat user names
+    val distinctUsers = elements.toStream.distinct.length
+
+    out.collect(WindowResult(
+      context.window.getStart,
+      context.window.getEnd,
+      key,
+      totalUpdates,
+      distinctUsers
+    ))
+  }
+}
 
 object KafkaReader {
   def main(args: Array[String]): Unit = {
@@ -23,10 +56,31 @@ object KafkaReader {
       .setValueOnlyDeserializer(new JsonDeserializer[WikipediaUpdate](classOf[WikipediaUpdate]))
       .build()
 
-    val lines = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
+    //val lines = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
 
-    
-    lines.print("OutputStream1").setParallelism(2)
+    val lines: DataStream[WikipediaUpdate] = env.fromSource(kafkaSource, 
+      WatermarkStrategy
+      .forBoundedOutOfOrderness(java.time.Duration.ofSeconds(5))
+      .withTimestampAssigner( new SerializableTimestampAssigner[WikipediaUpdate] {
+        override def extractTimestamp(element: WikipediaUpdate, recordTimestamp: Long): Long = element.timestamp * 1000
+      })
+      .withIdleness(java.time.Duration.ofSeconds(1))  // IMPORTANT: Handle idle sources
+      ,
+      "Kafka source 2"
+    )
+
+
+    val updates_by_wiki: DataStream[WindowResult] = lines
+      .keyBy(_.wiki)
+      .window(TumblingEventTimeWindows.of(Time.seconds(2)))
+      .process(new GatherRecords)
+      
+    // Group the updates by wiki and compute some stats
+    updates_by_wiki
+      .filter(_.wiki == "frwiki")
+      .map(t => s"${t.windowStart} (${t.wiki}) : ${t.totalUpdates} updates by ${t.distinctUsers} distinct users.")
+      .print("Wiki updates (grouped)")
+      .setParallelism(2)
 
 
     env.execute("Read from Kafka")
